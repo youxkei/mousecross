42dc0b8094e2a5b34ca4d94b82ba4d1d2fe85f45
youxkei <youxkei@gmail.com>
2020-12-29 01:55:20 +0900

    Handle mouse motion event in the window

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,46 @@
-use gio::ApplicationExt;
-use gtk::{WidgetExt, WindowExt};
+extern crate gtk;
+
+use cairo::{RectangleInt, Region};
+use gio::prelude::*;
+use gtk::prelude::*;
+
+use std::env::args;
 
 fn main() {
-    match gtk::Application::new(
-        "io.github.youxkei.mousecross",
-        gio::APPLICATION_HANDLES_OPEN,
-    ) {
-        Ok(app) => {
-            app.connect_activate(|app| {
-                let win = gtk::ApplicationWindow::new(&app);
-                win.set_title("Hello Gtk-rs");
-                win.show_all();
-            });
+    let app =
+        gtk::Application::new(Some("io.github.youxkei.mousecross"), Default::default()).unwrap();
 
-            app.run(&[""]);
-        }
-        Err(_) => {
-            println!("Application start up error");
-        }
-    };
+    app.connect_activate(|app| {
+        let win = gtk::ApplicationWindow::new(app);
+        win.set_title("Mouse Cross");
+
+        let rectangles = &[
+            RectangleInt {
+                x: 0,
+                y: 0,
+                width: 128,
+                height: 128,
+            },
+            RectangleInt {
+                x: 64,
+                y: 64,
+                width: 128,
+                height: 128,
+            },
+        ];
+        win.shape_combine_region(Some(&Region::create_rectangles(rectangles)));
+
+        win.show_all();
+
+        win.add_events(gdk::EventMask::POINTER_MOTION_MASK);
+        win.connect_motion_notify_event(
+            |_window: &gtk::ApplicationWindow, event: &gdk::EventMotion| {
+                println!("{:?}", event);
+
+                gtk::Inhibit(false)
+            },
+        );
+    });
+
+    app.run(&args().collect::<Vec<_>>());
 }
